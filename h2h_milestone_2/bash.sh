48bd07edc416ce0183a67a210ef8a1aae2b83755
jay <2005441@kiit.ac.in>
2023-06-03 19:01:27 +0530

    milestone 2

---
+++ h2h_milestone_2/bash.sh
@@ -0,0 +1,1 @@
+mysql -u username -p sales < ./database/hrc.sql
---
+++ h2h_milestone_2/src/com/highradius/connection/connection.java
@@ -0,0 +1,7 @@
+package com.highradius.connection;
+
+public class connection {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
